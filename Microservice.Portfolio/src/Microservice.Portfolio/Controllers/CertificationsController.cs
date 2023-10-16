9dcf19313dd8088be5da066e5111990968985578
Matheus Sexto <matheus.sexto@gmail.com>
2023-10-16 20:21:57 +0200

    test app configuration

--- Microservice.Portfolio/src/Microservice.Portfolio/Controllers/CertificationsController.cs
+++ Microservice.Portfolio/src/Microservice.Portfolio/Controllers/CertificationsController.cs
@@ -11,20 +11,33 @@ namespace Microservice.Portfolio.Controllers
     public class CertificationsController : ControllerBase
     {
         private readonly ILogger<CertificationsController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public CertificationsController(ILogger<CertificationsController> logger)
+        public CertificationsController(ILogger<CertificationsController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         [HttpGet(Name = "GetCertifications")]
         public IEnumerable<Certification> Get()
         {
-            string accountName = "blobportfolio";
-            string containerName = "portfolio";
+            string? accountName = _configuration["BlobConfigurations:AccountName"];
+            string? containerName = _configuration["BlobConfigurations:ContainerName"];
+            string? containerUrl = _configuration["BlobConfigurations:ContainerUrl"];
+            
+            if (string.IsNullOrEmpty(accountName))
+                throw new ArgumentNullException(nameof(accountName));
+            
+            if (string.IsNullOrEmpty(containerName))
+                throw new ArgumentNullException(nameof(containerName));
+            
+            if (string.IsNullOrEmpty(containerUrl))
+                throw new ArgumentNullException(nameof(containerUrl));
+
 
             // Construct the blob container endpoint from the arguments.
-            string containerEndpoint = string.Format("https://{0}.blob.core.windows.net/{1}", accountName, containerName);
+            string containerEndpoint = string.Format(containerUrl, accountName, containerName);
 
             // Get a credential and create a service client object for the blob container.
             BlobContainerClient containerClient = new BlobContainerClient(new Uri(containerEndpoint), new DefaultAzureCredential());
